5c6039d64605d4a8960d3b22673f2afba4eefc71
hungreebr <marketing@hungree.com.br>
2013-03-23 02:44:21 -0300

    Created rails app and add status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Petcol::Application.config.secret_token = '06b6ab5836902df34cac1b963259fb34d19cc69e99b23c939858aca61fb0e233c2160818c940e88a0a489f1485250e21001bd6d84cdc7f47672ccf33a07770df'
