f0a12e765c82e888efb04725309b71c1df61eb95
hungreebr <marketing@hungree.com.br>
2013-10-23 18:02:55 -0300

    precompile test

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require 'carrierwave'
-
+config.assets.precompile += ['webflow.css']
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
