33992b292491813f3a997c94161e014fecab463a
hungreebr <marketing@hungree.com.br>
2013-12-04 00:30:36 -0300

    adicionando topo da application e organizando css e javascripts

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ end
 	get 'register', to: 'devise/registrations#new', as: :register
 	get 'login', to: 'devise/sessions#new', as: :login
 	get 'logout', to: 'devise/sessions#destroy', as: :logout
-	 root to: 'devise/sessions#new', :layouts => "devise/sessions.html.erb" 
+	root to: 'home', to: 'static_pages#index', as: :home
   end
   
   resources :statuses 	
